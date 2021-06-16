c45deef3d228d36a2ef119ac33a584de6f66ca9f
anabeL19 <anabel.paredes@ucsp.edu.pe>
2021-06-16 17:01:47 -0500

    terraform files

---
+++ terraform.tfvars
@@ -0,0 +1,11 @@
+# Application Definition 
+app_name        = "terraform-test" #do NOT enter any spaces
+app_project     = "terraform-test-317000"
+
+# GCP Settings
+gcp_region_1  = "europe-west1"
+gcp_zone_1    = "europe-west1-b"
+gcp_auth_file = "terraform-test-317000-ebca6b95d0ef.json"
+
+# GCP Netwok
+private_subnet_cidr_1  = "10.10.1.0/24"
---
+++ variables-auth.tf
@@ -0,0 +1,19 @@
+# Google Cloud connection & authentication and Application configuration | variables-auth.tf
+
+# GCP authentication file
+variable "gcp_auth_file" {
+  type = string
+  description = "GCP authentication file"
+}
+
+# define GCP project name
+variable "app_project" {
+  type = string
+  description = "GCP project name"
+}
+
+# define application name
+variable "app_name" {
+  type = string
+  description = "Application name"
+}
